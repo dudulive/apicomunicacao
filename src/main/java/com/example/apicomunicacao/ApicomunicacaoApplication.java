53dc04c5b58fea56068f8fe3357a8d90758e6900
dudulive <edurodriguesbraz@hotmail.com>
2021-05-27 17:13:40 -0300

    Iniciando Projeto, adionado bibliotecas e configuração de banco de dados

---
+++ src/main/java/com/example/apicomunicacao/ApicomunicacaoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apicomunicacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApicomunicacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApicomunicacaoApplication.class, args);
+	}
+
+}
